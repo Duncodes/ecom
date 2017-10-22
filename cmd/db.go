bc802f5954c5d88b6d5d8cc4fcdcf333cb3807fd
Duncan Gitonga <duncangitonga292@gmail.com>
2017-10-22 10:27:04 +0300

    db command

---
+++ cmd/db.go
@@ -0,0 +1,23 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+func init() {
+	dbcmd.AddCommand(drop)
+}
+
+var dbcmd = &cobra.Command{
+	Use:   "db",
+	Short: "Manages ecom databases",
+	Long:  `The manages ecom database like a pro`,
+}
+
+var drop = &cobra.Command{
+	Use:   "drop",
+	Short: "drops all tables",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		// TODO run remove database command
+		database.DropTables()
+	},
+}
